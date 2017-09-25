12b0033f6e0dd66994e5586843fc00a57ba62ee1
szhao92 <shengkai@ShengkaimatoMacBook-Pro.local>
2017-09-24 22:44:47 -0700

    sdsdsdsd

--- js/bookmarklet_font.js
+++ js/bookmarklet_font.js
@@ -32,11 +32,10 @@ function initMyBookmarklet(){
 
         //YOUR CODE GOES HERE!
         if(image.initialHeight == null) { /* avoid accumulating integer-rounding error */ image.initialHeight=image.height; image.initialWidth=image.width; image.scalingFactor=1; } image.scalingFactor*=amt; image.width=image.scalingFactor*image.initialWidth; image.height=image.scalingFactor*image.initialHeight; } 
-                   var i,L=document.images.length; for (i=0;i<L;++i) zoomImage(document.images[i], 2); 
-                   if (!L) alert("This page contains no images."); 
-    
+        var i,L=document.images.length; for (i=0;i<L;++i) zoomImage(document.images[i], 2); 
+        if (!L) alert("This page contains no images."); 
         //YOUR CODE GOES HERE!
-})();   
+    })();
 
 }
 
