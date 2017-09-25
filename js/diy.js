98d17dff69e5ac2287b8a9a648c1f02db1f423ff
szhao92 <shengkai@ShengkaimatoMacBook-Pro.local>
2017-09-24 23:18:05 -0700

    Sdsdsd

--- js/diy.js
+++ js/diy.js
@@ -38,25 +38,19 @@ function initMyBookmarklet(){
         setTimeout(function() {rotateHeaders(++r % 360);}, 100);
       }
       rotateHeaders(1);
-      
-      })();
 
-
-    }
-function initMyBookmarklet(){
-    (window.myBookmarklet = function (){
-
-        //YOUR CODE GOES HERE!
-        var p=document.getElementsByTagName('*');
+       var p=document.getElementsByTagName('*');
         for(i=0;i<p.length;i++){
             if(p[i].style.fontSize){
                 var s=parseInt(p[i].style.fontSize.replace("px",""));
             } else {
-                var s=12;}s+=2;p[i].style.fontSize=s+"px"}
+                var s=12;}s+=2;p[i].style.fontSize=s+"px"};
 
-        //YOUR CODE GOES HERE!
-    })();
+      })();
 
-}
+
+
+    }
+
 
 })();
