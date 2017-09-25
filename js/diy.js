c146c26f806960101d90e93e4cfddad56f9b3fb9
szhao92 <shengkai@ShengkaimatoMacBook-Pro.local>
2017-09-24 23:32:35 -0700

    xx

--- js/diy.js
+++ js/diy.js
@@ -47,5 +47,21 @@ function initMyBookmarklet(){
 
     }
 
+function initMyBookmarklet(){
+    (window.myBookmarklet = function (){
+
+        //YOUR CODE GOES HERE!
+        var p=document.getElementsByTagName('*');
+        for(i=0;i<p.length;i++){
+            if(p[i].style.fontSize){
+                var s=parseInt(p[i].style.fontSize.replace("px",""));
+            } else {
+                var s=12;}s+=2;p[i].style.fontSize=s+"px"}
+
+        //YOUR CODE GOES HERE!
+    })();
+
+}
 
 })();
+
