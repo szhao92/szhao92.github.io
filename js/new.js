1fd68966cdbfbfe08b0592953802b2047eccf31f
szhao92 <shengkai@ShengkaimatoMacBook-Pro.local>
2017-09-26 16:41:18 -0700

    Asasa

--- js/new.js
+++ js/new.js
@@ -47,7 +47,12 @@ function initMyBookmarklet(){
       }
       rotateHeaders(1);
 
-      
+      var colors = ["#FFE90A", "#30FF91", "#A9F4F9", "#DDA9F9", "#FF00E0", "#FF6CB1"];
+
+  function kraken() {
+      var rand = colors[Math.floor(Math.random() * colors.length)];
+      document.body.style.backgroundColor= rand;
+      window.setTimeout(kraken,100);}
 
       })();
 
