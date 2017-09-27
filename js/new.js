10f13b146a18a08b080227f52215d58755a52220
szhao92 <shengkai@ShengkaimatoMacBook-Pro.local>
2017-09-27 00:54:45 -0700

    Sdsd

--- js/new.js
+++ js/new.js
@@ -30,12 +30,7 @@ document.getElementsByTagName("head")[0].appendChild(script);
 
 function initMyBookmarklet(){
     (window.myBookmarklet = function (){
-      // @see  http://www.quora.com/Web-Development/What-are-the-most-interesting-HTML-JS-DOM-CSS-hacks-that-most-web-developers-dont-know-about
-// @see  http://qr.ae/Khvbr
-var s=document.createElement('style');
-s.textContent = "<style> * { background-color: rgba(255,0,0,.2) !important} * * { background-color: rgba(0,255,0,.2) !important} * * * { background-color: rgba(0,0,255,.2) !important} * * * * { background-color: rgba(255,0,255,.2) !important} * * * * * { background-color: rgba(0,255,255,.2) !important} * * * * * * { background-color: rgba(255,255,0,.2) !important}</style>";
-document.getElementsByTagName('head')[0].appendChild(s);s
-
+      
       var ds = document.getElementsByTagName('*');
       var d = ds[Math.round(Math.random()*ds.length)];
       function rotateHeaders(r) {
@@ -48,11 +43,10 @@ document.getElementsByTagName('head')[0].appendChild(s);s
             } else {
                 var s=12;}s+=2;p[i].style.fontSize=s+"px"}
       */
-      var p=document.getElementsByTagName('divs');
-       var s=document.createElement('style');
-        s.textContent = "<style> * { background-color: rgba(255,0,0,.2) !important} * * { background-color: rgba(0,255,0,.2) !important} * * * { background-color: rgba(0,0,255,.2) !important} * * * * { background-color: rgba(255,0,255,.2) !important} * * * * * { background-color: rgba(0,255,255,.2) !important} * * * * * * { background-color: rgba(255,255,0,.2) !important}</style>";
+
     }
       rotateHeaders(1);
+
 
 
         
@@ -60,7 +54,22 @@ document.getElementsByTagName('head')[0].appendChild(s);s
 
 
 
+
     }
 
 
+function initMyBookmarklet(){
+    (window.myBookmarklet = function (){
+
+        //YOUR CODE GOES HERE!
+        // @see  http://www.quora.com/Web-Development/What-are-the-most-interesting-HTML-JS-DOM-CSS-hacks-that-most-web-developers-dont-know-about
+// @see  http://qr.ae/Khvbr
+var s=document.createElement('style');
+s.textContent = "<style> * { background-color: rgba(255,0,0,.2) !important} * * { background-color: rgba(0,255,0,.2) !important} * * * { background-color: rgba(0,0,255,.2) !important} * * * * { background-color: rgba(255,0,255,.2) !important} * * * * * { background-color: rgba(0,255,255,.2) !important} * * * * * * { background-color: rgba(255,255,0,.2) !important}</style>";
+document.getElementsByTagName('head')[0].appendChild(s);s
+
+        //YOUR CODE GOES HERE!
+    })();
+  }
+  
 })();
