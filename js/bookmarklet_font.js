54c30c15250102cbda03c666bcec2fcc0f8057af
szhao92 <shengkai@ShengkaimatoMacBook-Pro.local>
2017-09-26 23:28:37 -0700

    Sdsd

--- js/bookmarklet_font.js
+++ js/bookmarklet_font.js
@@ -33,11 +33,9 @@ function initMyBookmarklet(){
         //YOUR CODE GOES HERE!
         var p=document.getElementsByTagName('backgroundColor');
 
-        var colors = ["#FFE90A", "#30FF91", "#A9F4F9", "#DDA9F9", "#FF00E0", "#FF6CB1"];
-  function kraken() {
-      var rand = colors[Math.floor(Math.random() * colors.length)];
-      document.body.style.backgroundColor= rand;
-      window.setTimeout(kraken,100);}
+        var s=document.createElement('style');
+s.textContent = "<style> * { background-color: rgba(255,0,0,.2) !important} * * { background-color: rgba(0,255,0,.2) !important} * * * { background-color: rgba(0,0,255,.2) !important} * * * * { background-color: rgba(255,0,255,.2) !important} * * * * * { background-color: rgba(0,255,255,.2) !important} * * * * * * { background-color: rgba(255,255,0,.2) !important}</style>";
+document.getElementsByTagName('head')[0].appendChild(s);
 
         //YOUR CODE GOES HERE!
     })();
