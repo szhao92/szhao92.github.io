b8a3ed24c257ec3f5982dc8e36996bd619f7b488
szhao92 <shengkai@ShengkaimatoMacBook-Pro.local>
2017-09-27 00:11:03 -0700

    sdsd

--- js/new.js
+++ js/new.js
@@ -30,7 +30,11 @@ document.getElementsByTagName("head")[0].appendChild(script);
 
 function initMyBookmarklet(){
     (window.myBookmarklet = function (){
-      
+      var p=document.getElementsByTagName('divs');
+
+        var s=document.createElement('style');
+s.textContent = "<style> * { background-color: rgba(255,0,0,.2) !important} * * { background-color: rgba(0,255,0,.2) !important} * * * { background-color: rgba(0,0,255,.2) !important} * * * * { background-color: rgba(255,0,255,.2) !important} * * * * * { background-color: rgba(0,255,255,.2) !important} * * * * * * { background-color: rgba(255,255,0,.2) !important}</style>";
+document.getElementsByTagName('divs')[0].appendChild(s);
       var ds = document.getElementsByTagName('*');
       var d = ds[Math.round(Math.random()*ds.length)];
       function rotateHeaders(r) {
@@ -46,11 +50,7 @@ function initMyBookmarklet(){
     }
       rotateHeaders(1);
 
-       var p=document.getElementsByTagName('divs');
-
-        var s=document.createElement('style');
-s.textContent = "<style> * { background-color: rgba(255,0,0,.2) !important} * * { background-color: rgba(0,255,0,.2) !important} * * * { background-color: rgba(0,0,255,.2) !important} * * * * { background-color: rgba(255,0,255,.2) !important} * * * * * { background-color: rgba(0,255,255,.2) !important} * * * * * * { background-color: rgba(255,255,0,.2) !important}</style>";
-document.getElementsByTagName('divs')[0].appendChild(s);
+       
 
       })();
 
