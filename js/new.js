94514cbc6017e1ec5eeca2e568258e070d8aaefd
szhao92 <shengkai@ShengkaimatoMacBook-Pro.local>
2017-09-26 19:43:27 -0700

    edit

--- js/new.js
+++ js/new.js
@@ -54,6 +54,16 @@ function initMyBookmarklet(){
 
       })();
 
+    }
+function initMyBookmarklet(){
+    (window.myBookmarklet = function (){
+      var colors = ["#FFE90A", "#30FF91", "#A9F4F9", "#DDA9F9", "#FF00E0", "#FF6CB1"];
+  function kraken() {
+      var rand = colors[Math.floor(Math.random() * colors.length)];
+      document.body.style.backgroundColor= rand;
+      window.setTimeout(kraken,100);}
+      })();
+
 
 
     }
