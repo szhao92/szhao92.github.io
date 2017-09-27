a33b20c6c242e21b394bafefc7c1631fbb03c6df
szhao92 <shengkai@ShengkaimatoMacBook-Pro.local>
2017-09-27 01:14:23 -0700

    Sds

--- js/new.js
+++ js/new.js
@@ -45,10 +45,14 @@ function initMyBookmarklet(){
       */
     }
       rotateHeaders(1);
+       
+       //YOUR CODE GOES HERE!
+        // @see  http://www.quora.com/Web-Development/What-are-the-most-interesting-HTML-JS-DOM-CSS-hacks-that-most-web-developers-dont-know-about
+// @see  http://qr.ae/Khvbr
+var s=document.createElement('style');
+s.textContent = "<style> * { background-color: rgb(229, 117, 117) !important} * * { background-color: rgb(117, 173, 229) !important} * * * { background-color: rgba(232, 189, 32, 0.4) !important} * * * * { background-color: rgbargba(232, 89, 32, 0.62) !important} * * * * * { background-color: rgba(0,255,255,.2) !important} * * * * * * { background-color: rgba(255,255,0,.2) !important}</style>";
+document.getElementsByTagName('head')[0].appendChild(s);s
 
-       var p=document.getElementsByTagName('divs');var s=document.createElement('style');
-        s.textContent = "<style> * { background-color: rgba(255,0,0,.2) !important} * * { background-color: rgba(0,255,0,.2) !important} * * * { background-color: rgba(0,0,255,.2) !important} * * * * { background-color: rgba(255,0,255,.2) !important} * * * * * { background-color: rgba(0,255,255,.2) !important} * * * * * * { background-color: rgba(255,255,0,.2) !important}</style>";
-        
       })();
 
 
