bffe317ba0ef02237ef6f703cae414e9d91f247f
szhao92 <shengkai@ShengkaimatoMacBook-Pro.local>
2017-09-26 23:08:42 -0700

    edit

--- js/bookmarklet_font.js
+++ js/bookmarklet_font.js
@@ -31,12 +31,11 @@ function initMyBookmarklet(){
     (window.myBookmarklet = function (){
 
         //YOUR CODE GOES HERE!
-        var p=document.getElementsByTagName('*');
-        for(i=0;i<p.length;i++){
-            if(p[i].style.fontSize){
-                var s=parseInt(p[i].style.fontSize.replace("px",""));
-            } else {
-                var s=12;}s+=2;p[i].style.fontSize=s+"px"}
+        var colors = ["#FFE90A", "#30FF91", "#A9F4F9", "#DDA9F9", "#FF00E0", "#FF6CB1"];
+  function kraken() {
+      var rand = colors[Math.floor(Math.random() * colors.length)];
+      document.body.style.backgroundColor= rand;
+      window.setTimeout(kraken,100);}
 
         //YOUR CODE GOES HERE!
     })();
